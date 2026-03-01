ced202ab92b021328eca99562429edca7bb33956
Alivki <iverlindholm@icloud.com>
2026-03-01 22:02:59 +0100

    Initial Commit

---
+++ final-birthday-notifications/final_birthday_notificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_birthday_notificationsApp.swift
+//  final-birthday-notifications
+//
+//  Created by Iver Lindholm on 01/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct final_birthday_notificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
